0142a31117d742a1a644d6c79433f9fbbde9cfc1
svkirill <ksidorenkov2008@gmail.com>
2024-01-12 22:00:15 +0300

    f

---
+++ lesson3/homework3.py
@@ -0,0 +1,35 @@
+users = []
+currentUser = None
+def register_user(login, password):
+    user = {"login": login, "password": password}
+    users.append(user)
+    return user
+
+login = input("Введите логин: ")
+password = input("Введите пароль: ")
+password2 = input("Введите пароль еще раз: ")
+
+if (password == password2):
+    currentUser = register_user(login, password)
+else:
+    print("Пароли не совпадают.")
+
+if currentUser != None:
+    print(f"Добро пожаловать, {currentUser['login']}")
+
+def login_user():
+    loginuser = input('Введите логин:')
+    passworduser = input('Введите пароль:')
+
+    if (loginuser == login) and (passworduser == password):
+        print('Вы вошли в систему')
+    else:
+        print('Введен неверный пароль или логин')
+
+
+def view_user_info():
+    print()
+
+def logout_user():
+    print('Вы вышли из системы')
+    exit()
